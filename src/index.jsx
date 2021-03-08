6fc2e263212580e76b480cffed053ec1d20a10cf
wellington <wellingtonslpbi@hotmail.com>
2021-03-08 20:30:42 -0300

    add webpack config

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,4 @@
-import React from 'react'
+import {render} from 'react-dom'
+import App from './App'
 
-function App() {
-    return <h1>Hello World</h1>
-}
-
-export default App
+render(<App />, document.getElementById('root'))
