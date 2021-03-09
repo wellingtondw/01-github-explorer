6e385f1161e9ccb393b54915b89f485c5c2572ee
wellington <wellingtonslpbi@hotmail.com>
2021-03-08 21:04:09 -0300

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
